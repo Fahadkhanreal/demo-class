d6467548a4516f2e6788513567a455729825b678
Fahadkhanreal <fhadikhan00@gmail.com>
2024-10-24 20:36:43 +0500

    class code updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="bg-[url('/images/wallhaven-zx66lo_1920x1080.png bg-no-repeat bg-cover')] h-[2000px]">
       <h1 className="pt-[15] m-[10] bg-[#414141] text-[100px]     ">
-        hello
+        hello World
       </h1>
 
       <h2 className="h-22 bg-secondary">
