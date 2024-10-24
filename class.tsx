53966e612da0048d712dfe9c29d9e098cd86c2a7
Fahadkhanreal <fhadikhan00@gmail.com>
2024-10-24 20:35:14 +0500

    class code

---
+++ class.tsx
@@ -0,0 +1,36 @@
+// hackthone ma hama ui/ux banana hoga or vo bhot jaldi hona wala ha or us sa pehla class ma hackthone hoga jo usma acha kraga vo aga hackthone ma jayagaa
+
+// agr hama tailwind ma apne custom padding dene ha to vo hm p-[15px] to vo hm asa denga array ka ander  to tailwind ma hama apne hisab ha kuch dena ha to vo hm [] array ka ander denga 
+
+// or agr top bottm sa custom paddinf dena hoge to vo hm to py means top to bottom py-[10] or agr left to right to uska liya px or agr margin top to bottom to my-[10] or agr left to right to mx-[10] asa hm custom means apne mrzi ke pading or margin desakhta ha [] yeh laga kr.
+
+
+// or agr font size dena ha apne mrxzi sa to uska liya text-[100px] to text ka size 100 px hojayaga
+
+// hex code means jesa rgb hota ha red green blue vase css ma hota ha hex hex ma pehla hash ayaga uska bad six number yeh har color ka apna hota ha #414141.
+
+
+// px kya ha yeh ek unit jesa kg hota ha measure hota ha ab screen pr chote chote unit hote ha px means one pixel of unit hm px ka sath kabhi kabi pertacge deta ha percatntage jesa 30% to scren hundred pertentage ha to 30 parcent  uska to hm percentage sirf padding margin font inhe ma use kr sakte ha background ma nahi .
+
+
+// ek hota ha rem or em is sa website responsive bnte ha yeh body ka size sa kam krta ha isko bad ma smajhaga jb real world application banyga .
+
+
+// ab hm background image lagana seekhanga to image ma png jpeg svg hmna pehla src sa uper public ka foler banaya usma images ka folder uska ander pic rakhe then ham class denga usma bg-[url('/images')] hm public ka path nhi deta hm sirf images ka folder banaya to next.js kehta ha public ka ander ap jo bhe folder baniooge bs uska path dena ha public ka nahi public ka folder ko vo khud detect krleta ha isliya hm direct images ke path deta ha. to hmre image show hojate ha agr na show ho to hm height denga to ajaayage bg image
+
+
+
+// jb hm koi project banaate ha to hmre pass pehla sa theme customize krte ha primary colour secondary colour primry color ab hama primary color website pr bar bar use kra ha to uska liya hm tailwind.config.ts ma jayaga uska ander arhaaha ha content then uska ander arahe ha theme uska ander arha ha extend then usma mujha koi color add krna ha primary ka name sa hmna usma color dadiya asa primary: "#414141" ab hma use krna ha jaha use krna ha hm bs bg-primary karaga to vo hama show krdega ab hm webiste ma ek he colour use nhi krte ek hota ha primary or ek hota ha secondary 
+
+
+// rm -rf .next yeh hmra gitbash pr chalaga yeh hmre jo cache jo bana churo hoga yeh usa reset lrdega akser hang hota ha to set hojata ha isko hm bash ma chalayaga terminal pr.
+
+// design system likh la isko search krna ha next class ma padh kr ana ha 
+
+
+
+// github ko agr hama apna local ka sath ad krna ha to asa add karaga  git config --global user.name jo name ho github pr woh "Fahad khan" then check krna ka liya hm same command chalayaga git config --global user.name  then git config --global user.email jo email ho  yeh hm setup krte ha bs ek bar is ka bad hama bs kuch comand chalne ha 
+
+// uska bad hma jes koi code dalna ha github pr to hm code ka terminal open krka git init karaga is sa hmre pass git initalised hogya to github pr el icon ha git ka icon ha hm jb git init karag to vo sare file usma ajaayga 3 number jo ha side bar ma iska bad file ko save krna ha or stage krna ha hmna save krle uska bad command likha git add . satge sa  mans hmre kuch file ha jo git ka system ma add nhi islliya hmm stage krte ha git add . asa iska bad hm check karaga git status sare file green hojayaga pehla kuch file red horhe the qk hmna stage nhi kiya tha ab agr hma git ma agr koi comment add krna jesa class ka code ha to git commit -m "class code updated" asa hm commit add krte ha git ma  ab git commit krna ka bad mana file ma change kiya to git ka jo icon ha vs code ma vo dekhayaga ka ek chez changes hiye ha yeh dekhata ha to ab achange krna ka bad hm again changes ko add karaga jesa change karaga bar bar stage karaga or bar bar commit karaga  jb bhe changr kraga to stage karaga git add . then commit then satus karaga to vo show karaga no tree ab hm jayaga github pr or waha create repo karag repo name hmna banye demo name or then description banaye ska bad repo public ab hmre pass repo create hogye but vo rempty ha ab uska bad hm kesa push karaga code last 3 chalayaga ab hm    .1 git branch -M main is sa hm branch ko main krdenga  ab    2. git remote add origin https//github.com/realfahadkhan to hmnre jo local git ha woh or hmre gitub ke repo sa add bhe to karvayaga  uska bad hm check karaga git remove -v to dono ka url show karaga fetch or push to hmre local sa gitub add hogye ab bs hm apna code push karaga ab last ha git push -u origin main bs yeh last command ka bad hmre code push hojayagaa git push karna ka bad git status check karaga your branch is up to date 
+
+//first command git init  uska bad file save then stage krna ka liya second command git add . 
